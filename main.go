53cb0a38febe14cbea85be1c6b499a123ba85e33
AbdeBMZZ <abdellahboumaiza1@gmail.com>
2022-08-29 13:19:51 +0100

    struct

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strconv"
 )
 
 const conferenceTickets int = 50
 var remainingTickets uint = 50
-var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName string
+	lastName string
+	email string
+	tickets uint
+}
 
 func main() {
 
@@ -56,7 +62,7 @@ func main() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -90,12 +96,12 @@ func bookTicket ( userTickets uint, firstName string, lastName string, email str
 	remainingTickets -= userTickets
 
 	//creating map
-	userData := make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["tickets"] = strconv.FormatUint(uint64(userTickets), 10)
-
+	var userData = UserData{
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		tickets: userTickets,
+	}
 
 	bookings = append(bookings, userData)
 	fmt.Println("list of bookings : \n", bookings)
